d972489574b1a1d4869bab63e2676fb8ce0cb5a4
Jonny.Hong <jonny.hong91@gmail.com>
2016-04-29 19:54:15 +0800

    board1 / 1 4 5 6 7

--- code/board1/main.c
+++ code/board1/main.c
@@ -6,8 +6,11 @@
 sbit STEP_0_SIGNAL = P0^5;	//第一关开启 关门
 sbit STEP_1_SIGNAL = P0^0;	  //第一关完成
 sbit STEP_2_SIGNAL = P0^4;	   //第二个完成 霍尔开关的接入
-sbit STEP_5_LINE_SIGNAL = P4^0; //16个点连线接入
-sbit STEP_5_TO_6_DOOR =  P4^2; //第五关完成的电磁锁 
+sbit STEP_5_SIGANL; // 
+sbit STEP_6_LINE_SIGNAL = P4^0; //16个点连线接入
+sbit STEP_6_TO_7_DOOR =  P4^2; //第六关完成的电磁锁
+sbit STEP_7_DOOR;
+sbit STEP_7_SIGNAL; 
 //当前的步骤 没有参与过 = 0
 signed char step = 0;
 
@@ -27,7 +30,7 @@ void main()
 				step = 2; 
 				delay_ms(5000);
 				
-				playMp3(0x00,0x01);	   //播放第一个文件夹的第一个音乐
+				play_mp3(0x00,0x01);	   //播放第一个文件夹的第一个音乐
 			} 
 		}
 		//第一关由敲门开关控制 直接就算完成了 
@@ -38,22 +41,49 @@ void main()
 				step = 5; //进去第5个谜题 
 				
 				delay_ms(5000);
-				playMp3(0x00,0x02);
+				play_mp3(0x00,0x02);
 			}
 		}
-		
-		if(step == 5) //机关5 接电线 
+
+		if(step == 5)
 		{
-			if(STEP_5_LINE_SIGNAL == 1)
-			{
+			if(STEP_6_LINE_SIGNAL == 1)
+			{	
 				delay_ms(500);
-				if(STEP_5_LINE_SIGNAL == 1) //接线成功 第5关完成 
+				if(STEP_5_SIGANL == 1)
 				{
 					step = 6;
-					playMp3(0x00,0x02);
-				 	STEP_5_TO_6_DOOR = 0; //电磁锁开门 
+				} 
+			} 
+		}
+		
+		if(step == 6) //机关5 接电线 
+		{
+			if(STEP_6_LINE_SIGNAL == 1)
+			{
+				delay_ms(500);
+				if(STEP_6_LINE_SIGNAL == 1) //接线成功 第6关完成 
+				{
+					step = 7;
+					play_mp3(0x00,0x02);
+				 	STEP_6_TO_7_DOOR = 0; //电磁锁开门 
 				}
 			} 
+		}
+		
+		if(step == 7)//茶会解密
+		{
+			if(STEP_7_SIGNAL == 1)
+			{
+				delay_ms(500);
+				if(STEP_7_SIGNAL == 1)
+				{
+					step = 8;
+					play_mp3(0x00,0x02);
+					
+					STEP_7_DOOR = 0; //打开7的门 
+				}
+			}
 		} 
 	}
 }
