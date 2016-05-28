95da4ecfc3ac764e006b29bd416c9b362758368d
Jonny <jonny.hong91@gmail.com>
2016-05-29 01:18:58 +0800

    板2 3 调试

--- code/board2/main.c
+++ code/board2/main.c
@@ -3,36 +3,34 @@
 #include "uart.h" 
 #include "mp3.h"
 
-//两个继电器
-sbit INPUT_00 = P0^0;
-sbit INPUT_01 = P0^1;
+//两个继电器 输入
+sbit INPUT_40 = P4^0;
+sbit INPUT_20 = P2^0;
+
 //4个信箱 继电器
+sbit INPUT_44 = P4^4; 
+sbit INPUT_45 = P4^5; 
+sbit INPUT_01 = P0^1; 
 sbit INPUT_02 = P0^2; 
-sbit INPUT_03 = P0^3; 
-sbit INPUT_04 = P0^4; 
-sbit INPUT_05 = P0^5; 
 
 //接电线继电器
-sbit INPUT_20 = P2^0;
 sbit INPUT_21 = P2^1;
-sbit INPUT_22 = P2^2;
-sbit INPUT_23 = P2^3;
-sbit INPUT_24 = P2^4;
-sbit INPUT_25 = P2^5;
-sbit INPUT_26 = P2^6;
-sbit INPUT_27 = P2^7;
 
-//接电线 推杆
-sbit OUTPUT_34 = P3^4; 
-
-//信封 电磁锁
+//信封 + 接电线 电磁锁开关
 sbit OUTPUT_43 = P4^3; 
+
+//最后电磁锁
+sbit OUTPUT_17 = P1^7;
 
 char step = 0;
 void INIT_COM();
 
 char message[] = {0,0,0,0};
 char i = 0;
+
+char step1_0 = 0;
+char step1_1 = 0; //两个门禁
+char step1_2 = 0;//接电线
 
 void main()
 {
@@ -41,52 +39,80 @@ void main()
 	uart_init();
 	mp3_init();
 	
+	play_mp3(0,1);
+
 	while(1)
 	{
 		if(step == 0)
 		{
-			if(INPUT_00 == 0 && INPUT_01 == 0)
+			if( (step1_0 == 0) && (INPUT_40 == 0))
 			{
-				step = 1;
+				delay_ms(50);
+				if( (step1_0 == 0) && (INPUT_40 == 0))
+				{
+					step1_0 = 1;
+					play_mp3(0,1);
+				}
 			}
-		}
 
-		//接电线
-		if(step == 1)
-		{
-			if(INPUT_20 == 0 && INPUT_21 == 0 && INPUT_22 == 0 && INPUT_23 == 0 && INPUT_24 == 0 && INPUT_25 == 0 && INPUT_26 == 0 && INPUT_27 == 0)
+			if( (step1_1 == 0) && (INPUT_20 == 0))
 			{
-				OUTPUT_34 = 0;
-				step = 2;
+				delay_ms(50);
+				if( (step1_1 == 0) && (INPUT_20 == 0))
+				{
+					step1_1 = 1;
+					play_mp3(0,2);
+				}
+			}
+
+			if((step1_2 == 0)&&(INPUT_21 == 0))
+			{
+				delay_ms(50);
+				if((step1_2 == 0)&&(INPUT_21 == 0))
+				{
+					step1_2 = 1;
+					play_mp3(0,3);
+				}
+			}
+
+			if((step1_0 == 1)&&(step1_1 == 1)&&(step1_2==1))
+			{
+				OUTPUT_43 = 0;
+				step = 1;
+				play_mp3(0,4);
 			}
 		}
 		
-		if(step == 2)
+		//信封
+		if(step == 1)
 		{
-			if(INPUT_02 == 0)
+			if(INPUT_44 == 0)
 			{
 				message[0] = 1;
+				play_mp3(0,1);
 			}
-			if(INPUT_03 == 0)
+			if(INPUT_45 == 0)
 			{
 				message[1] = 1;
+				play_mp3(0,2);
 			}
-			if(INPUT_04 == 0)
+			if(INPUT_01 == 0)
 			{
 				message[2] = 1;
+				play_mp3(0,3);
 			}
-			if(INPUT_05 == 0)
+			if(INPUT_02 == 0)
 			{
 				message[3] = 1;
+				play_mp3(0,4);
 			}
 
-			for(i = 0 ; i < 4 ; i++)
+
+			if((message[0]==1)&&(message[1]==1)&&(message[2]==1)&&(message[3]==1))
 			{
-				if(i == 3 && message[i] == 1)
-				{
-					step = 3; //信封完成 
-					OUTPUT_43 = 0;//电磁锁 断电 
-				}
+				 step = 2;		 //信封完成
+				 OUTPUT_17 = 0;
+				 play_mp3(0,5);
 			}
 		} 
 	} 
@@ -96,24 +122,15 @@ void main()
 //初始化所有口
 void INIT_COM()
 {
-	INPUT_00 = 1;
-	INPUT_01 = 1;
-
+	OUTPUT_43 = 1;
+	INPUT_40 = 1;
 	INPUT_20 = 1;
 	INPUT_21 = 1;
-	INPUT_22 = 1;
-	INPUT_23 = 1;
-	INPUT_24 = 1;
-	INPUT_25 = 1;
-	INPUT_26 = 1;
-	INPUT_27 = 1;
-	
-	OUTPUT_34 = 1;
 
+	INPUT_44 = 1;
+	INPUT_45 = 1;
+	INPUT_01 = 1;
 	INPUT_02 = 1;
-	INPUT_03 = 1;
-	INPUT_04 = 1;
-	INPUT_05 = 1;
-	
-	OUTPUT_43 = 1;
+
+	OUTPUT_17 = 1;
 }
--- code/board3/main.c
+++ code/board3/main.c
@@ -5,49 +5,50 @@
 #include "mp3.h"
 
 //4个按钮输入 
-sbit INPUT_06 = P0^6;
-sbit INPUT_07 = P0^7;
-sbit INPUT_46 = P4^6;
-sbit INPUT_41 = P4^1;
+sbit INPUT_04 = P0^4;
+sbit INPUT_03 = P0^3;
+sbit INPUT_02 = P0^2;
+sbit INPUT_01 = P0^1;
 
-sbit INPUT_05 = P0^5; 
+//霍尔开关输入
+sbit INPUT_00 = P0^0; 
 
 //涂玫瑰 电磁锁
-sbit OUTPUT_43 = P4^3;
+sbit OUTPUT_21 = P2^1;
 //最后一个电磁锁
-sbit OUTPUT_17 = P1^7;
+sbit OUTPUT_22 = P2^2;
 
 //接灯带
 //第一个灯:
 //RGB
-sbit OUTPUT_00 = P0^0;
-sbit OUTPUT_01 = P0^1;
-sbit OUTPUT_02 = P0^2;
+sbit OUTPUT_43 = P4^3;
+sbit OUTPUT_17 = P1^7;
+sbit OUTPUT_16 = P1^6;
 //第二个灯:
 //RGB
-sbit OUTPUT_03 = P0^3;
-sbit OUTPUT_04 = P0^4;
-sbit OUTPUT_45 = P4^5;
+sbit OUTPUT_15 = P1^5;
+sbit OUTPUT_14 = P1^4;
+sbit OUTPUT_13 = P1^3;
 //第三个灯:
 //RGB
-sbit OUTPUT_27 = P2^7;
-sbit OUTPUT_26 = P2^6;
-sbit OUTPUT_25 = P2^5;
+sbit OUTPUT_12 = P1^2;
+sbit OUTPUT_11 = P1^1;
+sbit OUTPUT_10 = P1^0;
 //第四个灯:
 //RGB
+sbit OUTPUT_42 = P4^2;
 sbit OUTPUT_40 = P4^0;
-sbit OUTPUT_20 = P2^0;
-sbit OUTPUT_21 = P2^1;  
+sbit OUTPUT_20 = P2^0;  
 
 char step = 0; 
 
 //RGB RED:011b = 3	低电平亮
 char color[] = {0,1,2,3,4,5,6,7}; //8种颜色
 //初始化都灭
-char color_step_0 = 7;
-char color_step_1 = 7;
-char color_step_2 = 7;
-char color_step_3 = 7;
+char color_step_0 = 2;
+char color_step_1 = 5;
+char color_step_2 = 1;
+char color_step_3 = 0;
 
 void light(char lightNumber , char color);
 void INIT_COM();
@@ -63,10 +64,10 @@ void main()
 	{
 		if(step == 0)
 		{
-			if(INPUT_06 == 0)
+			if(INPUT_04 == 0)
 			{
 				delay_ms(50);
-				if(INPUT_06 == 0)
+				if(INPUT_04 == 0)
 				{
 					color_step_0 = (++color_step_0)%8;
 					color_step_1 = (++color_step_1)%8;
@@ -75,10 +76,10 @@ void main()
 				}
 			} 
 			
-			if(INPUT_07 == 0)
+			if(INPUT_03 == 0)
 			{
 				delay_ms(50);
-				if(INPUT_07 == 0)
+				if(INPUT_03 == 0)
 				{
 					color_step_0 = (++color_step_0)%8;
 					color_step_1 = (++color_step_1)%8;	
@@ -89,10 +90,10 @@ void main()
 				}
 			} 
 			
-			if(INPUT_46 == 0)
+			if(INPUT_02 == 0)
 			{
 				delay_ms(50);
-				if(INPUT_46 == 0)
+				if(INPUT_02 == 0)
 				{
 					color_step_1 = (++color_step_1)%8; 
 					color_step_2 = (++color_step_2)%8;	
@@ -104,10 +105,10 @@ void main()
 				}
 			} 
 			
-			if(INPUT_41 == 0)
+			if(INPUT_01 == 0)
 			{
 				delay_ms(50);
-				if(INPUT_41 == 0)
+				if(INPUT_01 == 0)
 				{
 					color_step_2 = (++color_step_2)%8;
 					color_step_3 = (++color_step_3)%8;
@@ -118,20 +119,20 @@ void main()
 			
 			if(color_step_0 == 4 && color_step_1 == 4 && color_step_2 == 4 && color_step_3 == 4)
 			{
-				step = 2;
-				OUTPUT_43 = 0 ;//打开电磁锁 
+				step = 1;
+				OUTPUT_21 = 0 ;//打开电磁锁 
 			}
 		}
 		
-		if(step == 2)
+		if(step == 1)
 		{
-			if(INPUT_05 == 0)
+			if(INPUT_00 == 0)
 			{
 				delay_ms(50);
-				if(INPUT_05 == 0)
+				if(INPUT_00 == 0)
 				{
-					step = 3; 
-					OUTPUT_17 = 0;//最后一个门 
+					step = 2; 
+					OUTPUT_22 = 0;//最后一个门 
 				} 
 			} 
 		} 
@@ -144,33 +145,33 @@ void light(char lightNumber , char color)
 	{
 		case 0:
 		{
-			OUTPUT_00 = (color & 4)>>2; 
-			OUTPUT_01 = (color & 2)>>1;
-			OUTPUT_02 = color & 1;	
+			OUTPUT_43 = (color & 4)>>2; 
+			OUTPUT_17 = (color & 2)>>1;
+			OUTPUT_16 = color & 1;	
 			break;
 		}
 		
 		case 1:
 		{
-			OUTPUT_03 = (color & 4)>>2; 
-			OUTPUT_04 = (color & 2)>>1;
-			OUTPUT_25 = color & 1;		
+			OUTPUT_15 = (color & 4)>>2; 
+			OUTPUT_14 = (color & 2)>>1;
+			OUTPUT_13 = color & 1;		
 			break;
 		}
 		
 		case 2:
 		{
-			OUTPUT_27 = (color & 4)>>2; 
-			OUTPUT_26 = (color & 2)>>1;
-			OUTPUT_25 = color & 1;	
+			OUTPUT_12 = (color & 4)>>2; 
+			OUTPUT_11 = (color & 2)>>1;
+			OUTPUT_10 = color & 1;	
 			break;
 		}
 		
 		case 3:
 		{
-			OUTPUT_40 = (color & 4)>>2; 
-			OUTPUT_20 = (color & 2)>>1;
-			OUTPUT_21 = color & 1;	
+			OUTPUT_42 = (color & 4)>>2; 
+			OUTPUT_40 = (color & 2)>>1;
+			OUTPUT_20 = color & 1;	
 			break;
 		}
 	}
@@ -178,28 +179,28 @@ void light(char lightNumber , char color)
 
 void INIT_COM()
 {
-	INPUT_06 = 1;
-	INPUT_07 = 1; 
-	INPUT_46 = 1; 
-	INPUT_41 = 1;
-	INPUT_05 = 1;
+	INPUT_04 = 1;
+	INPUT_03 = 1; 
+	INPUT_02 = 1; 
+	INPUT_01 = 1;
+	INPUT_00 = 1;
 	
-	OUTPUT_43 = 1;
+	OUTPUT_21 = 1;
 	OUTPUT_17 = 1;
 	
-	OUTPUT_00 = 1; 
-	OUTPUT_01 = 1;
-	OUTPUT_02 = 1;
+	OUTPUT_43 = 1; 
+	OUTPUT_17 = 1;
+	OUTPUT_16 = 1;
 	
-	OUTPUT_03 = 1;
-	OUTPUT_04 = 1;
-	OUTPUT_25 = 1;
+	OUTPUT_15 = 1;
+	OUTPUT_14 = 1;
+	OUTPUT_13 = 1;
 	
-	OUTPUT_27 = 1;
-	OUTPUT_26 = 1;
-	OUTPUT_25 = 1;
+	OUTPUT_12 = 1;
+	OUTPUT_11 = 1;
+	OUTPUT_10 = 1;
 	
+	OUTPUT_42 = 1;
 	OUTPUT_40 = 1;
 	OUTPUT_20 = 1;
-	OUTPUT_21 = 1;
 }
